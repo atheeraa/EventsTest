5636e082924aee8f6c11503af9807f28c505c8dc
Atheer <atheerabdullaha@outlook.com>
2019-09-22 14:28:10 +0300

    initial commit

---
+++ app/src/main/java/com/example/work/test/AboutActivity.java
@@ -0,0 +1,43 @@
+package com.example.work.test;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class AboutActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+}
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.about:
+                    Intent a = new Intent(AboutActivity.this,MainActivity.class);
+                    startActivity(a);
+                    mTextMessage.setText(R.string.about);
+                    break;
+            }
+            return false;
+        }
+    };
+
+
+
+    }
---
+++ app/src/main/java/com/example/work/test/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.work.test;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.about:
+                    Intent a = new Intent(MainActivity.this,AboutActivity.class);
+                    startActivity(a);
+                    mTextMessage.setText(R.string.about);
+                    break;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        mTextMessage.setTextSize(32);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
